5c4c291bfad386466465456f3fe8a8e4f44ec0ef
aayushmpatra <aayushmpatra2025@gmail.com>
2026-06-17 18:15:57 -0500

    added core course logic
    crated isUnlocked to check if course is available to take, then push each course into their appropriate arrays

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,28 @@
+import COURSES from './COURSES.json';
+
+const takenCourses = new Set();
+
+function isUnlocked(courseId) {
+  return COURSES[courseId].prereqs.every(prereq => takenCourses.has(prereq))
+}
+
+
+
 function App() {
+  const availablCourses = [];
+  const lockedCourses = [];
+
+  for (const courseId of Object.keys(COURSES)) {
+    if (takenCourses.has(courseId)) {
+      continue
+    }
+    if (isUnlocked(courseId)) {
+      availablCourses.push(courseId);
+    } else {
+      lockedCourses.push(courseId);
+    }
+  }
+
   return <h1>Degree Tree</h1>
 }
 
