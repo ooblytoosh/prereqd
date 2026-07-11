5ef8e46371746382302c9e671026fcd8042c40e4
aayushmpatra <aayushmpatra2025@gmail.com>
2026-07-11 15:29:48 -0500

    added credit hours to course cards

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -12,6 +12,7 @@ export function CourseCard({courseId, status, onClick}) {
     <div className={`course-card ${status}`} onClick={onClick}>
       <p className='courseId'>{courseId}</p>
       <p className='courseTitle'>{course.name}</p>
+      <p className='creditHours'>{course.hours} credits</p>
       <p className='status'>Status: {status}</p>
     </div>
   )
